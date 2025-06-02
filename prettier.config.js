6b845c2842efd24329d2fb0e89da523e7bd71840
Hung Nguyen <nthung2112@gmail.com>
2025-06-02 16:36:40 +0700

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,29 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  endOfLine: "lf",
+  semi: true,
+  trailingComma: "es5",
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrderCaseSensitive: true,
+  importOrder: [
+    "^(react|react-dom)(/.+)?$",
+    "^(@tanstack/)(/.+)?$",
+    "^(@?react-).+",
+    "<THIRD_PARTY_MODULES>", // third party modules
+    "",
+    "^types$", // types
+    "",
+    "^(@/|~/)", // local modules (starting with @/ or ~/)
+    "",
+    "^[./]", // relative imports
+  ],
+};
+
+export default config;
